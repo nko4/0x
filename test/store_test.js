faa342c72445eb6442a7a94792c0af52b4ced26a
roylines <roy@roylines.co.uk>
2013-11-09 13:09:20 +0000

    integration with lanyrd

--- test/store_test.js
+++ test/store_test.js
@@ -6,7 +6,7 @@ describe('store', function() {
     store.cache = {
     };
   });
-  it('should return expected for nodeconfeu', function(done) {
+  it.skip('should return expected for nodeconfeu', function(done) {
     var conference = {
       id: 'nodeconfeu:2013',
       slug: 'nodeconfeu',
@@ -16,6 +16,21 @@ describe('store', function() {
       assert(!e);
       assert.equal(d.conference.id, 'nodeconfeu:2013');
       assert.equal(d.attendees.length, 42);
+      return done(e);
+    });
+  });
+  it('should return expected for jsconfeu', function(done) {
+    var conference = {
+      id: 'jsconfeu:2013',
+      slug: 'jsconfeu',
+      year: '2013'
+    };
+    store.getConference(conference, function(e, d) {
+      assert(!e);
+      assert.equal(d.conference.id, 'jsconfeu:2013');
+      assert.equal(d.conference.lat, 52.5106);
+      assert.equal(d.conference.lng, 13.4287);
+      assert.equal(d.attendees.length, 104);
       return done(e);
     });
   });
