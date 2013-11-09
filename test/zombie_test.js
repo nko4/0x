6828999a1b995f57455672def7716c38e4333d9c
jamesbloomer <github2@thebloomers.co.uk>
2013-11-09 22:20:56 +0000

    cope when everyone is a zombie

--- test/zombie_test.js
+++ test/zombie_test.js
@@ -10,5 +10,20 @@ describe('zombie', function() {
             var behaviours = z.getBehaviours();
             assert.equal(behaviours.length, 2);
         });
+
+        it('should not error when everyone is a zombie', function(done) {
+            var pp = new Person('TEST1', 1, 2);
+            var zz = new Zombie(pp);
+            var p = new Person('TEST2', 1, 2);
+            var z = new Zombie(p);
+            var people = [];
+            people.push(zz);
+            people.push(z);
+            var behaviours = z.getBehaviours();
+            behaviours[0](z, people, function(e) {
+                assert.equal(e, null);
+                return done();
+            });
+        });
     });
 });
