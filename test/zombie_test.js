73db8c56049596cebb4312204e37a3fbdcd85db7
jamesbloomer <github2@thebloomers.co.uk>
2013-11-09 22:02:22 +0000

    add a zombie test

--- test/zombie_test.js
+++ test/zombie_test.js
@@ -1,1 +1,14 @@
- // TODO seek 
+var assert = require('assert'),
+    Person = require('../lib/person'),
+    Zombie = require('../lib/zombie');
+
+describe('zombie', function() {
+    describe('getBehaviours', function() {
+        it('should have two behaviours', function() {
+            var p = new Person('TEST', 1, 2);
+            var z = new Zombie(p);
+            var behaviours = z.getBehaviours();
+            assert.equal(behaviours.length, 2);
+        });
+    });
+});
