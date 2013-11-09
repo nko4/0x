89f983b6fa01c093adff6d4b2d29aa7971bef05e
jamesbloomer <github2@thebloomers.co.uk>
2013-11-09 06:55:24 +0000

    implement start and stop on engine #2

---
+++ test/engine_test.js
@@ -0,0 +1,41 @@
+var assert = require('assert'),
+    engine = require('../lib/engine');
+
+describe('engine', function() {
+    beforeEach(function() {
+        engine.conferences = {};
+    });
+
+    describe('start', function() {
+        it('should create new conference', function(done) {
+            engine.start({
+                id: 'TEST'
+            }, function(e) {
+                assert.equal(Object.keys(engine.conferences).length, 1);
+                assert(engine.conferences.TEST);
+                return done();
+            });
+        });
+    });
+
+    describe('stop', function() {
+        it('should destroy conference', function(done) {
+            var data1 = {id: '1'};
+            var data2 = {id: '2'};
+            var data3 = {id: '3'};
+
+            engine.start(data1, function() {
+                engine.start(data2, function() {
+                    engine.start(data3, function() {
+                        engine.stop('2', function(e) {
+                            assert.equal(Object.keys(engine.conferences).length, 2);
+                            assert.equal(engine.conferences['1'].id, '1');
+                            assert.equal(engine.conferences['3'].id, '3');
+                            return done();
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
