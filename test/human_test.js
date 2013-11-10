93c4d5e9557570aa9bea92188875367a34969094
jamesbloomer <github2@thebloomers.co.uk>
2013-11-10 09:46:53 +0000

    humans seek beer

---
+++ test/human_test.js
@@ -0,0 +1,14 @@
+var assert = require('assert'),
+    Person = require('../lib/person'),
+    Human = require('../lib/human');
+
+describe('human', function() {
+    describe('getBehaviours', function() {
+        it('should have behaviours', function() {
+            var p = new Person('TEST', 1, 2);
+            var h = new Human(p);
+            var behaviours = h.getBehaviours();
+            assert.equal(behaviours.length, 2);
+        });
+    });
+});
